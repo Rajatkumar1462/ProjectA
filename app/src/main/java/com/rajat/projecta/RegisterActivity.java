f3b1f05aa5268209696ce6f31ff359fc2b03d496
Rajat <rajatleuva1462@gmail.com>
2021-02-11 19:02:51 +0530

    RegisterActivity.java added with related files

--- app/src/main/java/com/rajat/projecta/RegisterActivity.java
+++ app/src/main/java/com/rajat/projecta/RegisterActivity.java
@@ -1,15 +1,97 @@
 package com.rajat.projecta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText email;
+    EditText password;
+    Button registerBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        registerBtn = findViewById(R.id.registerBtn);
+        mAuth = FirebaseAuth.getInstance();
+        final String TAG = "RegisterActivity";
+
+
+        // checking if user already logged in or not
+        if(isLoggedIn()){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeKeyboard();
+                email.clearFocus();
+                password.clearFocus();
+
+                //sign in method
+                String Email = email.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+                mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //sign in successful then update UI
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else{
+                            Toast.makeText(RegisterActivity.this, "Authentication failed with following message /n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+    // check if user already logged in
+    public boolean isLoggedIn() {
+        return mAuth.getCurrentUser() != null;
+    }
+
+    //updating if user successfully registered
+    public void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Toast.makeText(this, "Welcome "+user.getEmail(), Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        } else {
+            Toast.makeText(this, "Username can't be null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    final void closeKeyboard(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
 }
