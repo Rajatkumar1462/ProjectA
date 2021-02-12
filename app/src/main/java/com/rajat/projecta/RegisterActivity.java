929e33aa1ed76a529e78bc1dfb0b7c66900b5964
Rajat <rajatleuva1462@gmail.com>
2021-02-12 09:46:08 +0530

    Added some constraints on RegisterActivity.java

--- app/src/main/java/com/rajat/projecta/RegisterActivity.java
+++ app/src/main/java/com/rajat/projecta/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,14 +18,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    EditText email;
-    EditText password;
-    Button registerBtn;
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private Button registerBtn;
+    private EditText userName;
+    private TextView loginLink;
+    private TextView topregisterBtn;
+    private DatabaseReference mDatabase;
 
+    final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +41,11 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.registerEmail);
         password = findViewById(R.id.registerPassword);
         registerBtn = findViewById(R.id.registerBtn);
+        userName = findViewById(R.id.registerUsername);
+        loginLink = findViewById(R.id.LoginBtnOnRegister);
+        topregisterBtn = findViewById(R.id.registerBtn2);
         mAuth = FirebaseAuth.getInstance();
-        final String TAG = "RegisterActivity";
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
 
         // checking if user already logged in or not
@@ -43,6 +54,7 @@ public class RegisterActivity extends AppCompatActivity {
             finish();
         }
 
+        // clicking activity on Create Account Button
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,20 +63,80 @@ public class RegisterActivity extends AppCompatActivity {
                 password.clearFocus();
 
                 //sign in method
-                String Email = email.getText().toString().trim();
-                String Password = password.getText().toString().trim();
-                mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            //sign in successful then update UI
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else{
-                            Toast.makeText(RegisterActivity.this, "Authentication failed with following message /n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                final String Email = email.getText().toString().trim();
+                final String Password = password.getText().toString().trim();
+                final String UserId = userName.getText().toString().trim();
+
+                if(Email.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Email Field can't be empty...", Toast.LENGTH_SHORT).show();
+                else if(Password.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Password can't be empty...", Toast.LENGTH_SHORT).show();
+                else if(UserId.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "UserName can't be empty...", Toast.LENGTH_SHORT).show();
+                else{
+                    mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                //If sign in successful then update UI
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                writeNewUser(UserId, Email, Password);
+                                updateUI(user);
+                            } else{
+                                Toast.makeText(RegisterActivity.this, "Authentication failed with following message /n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
+            }
+        });
+
+
+        //clicking activity on top situated register account button
+        topregisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeKeyboard();
+                email.clearFocus();
+                password.clearFocus();
+
+                //sign in method
+                final String Email = email.getText().toString().trim();
+                final String Password = password.getText().toString().trim();
+                final String UserId = userName.getText().toString().trim();
+
+                if(Email.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Email Field can't be empty...", Toast.LENGTH_SHORT).show();
+                else if(Password.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Password can't be empty...", Toast.LENGTH_SHORT).show();
+                else if(UserId.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "UserName can't be empty...", Toast.LENGTH_SHORT).show();
+                else{
+                    mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                //If sign in successful then update UI
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                writeNewUser(UserId, Email, Password);
+                                updateUI(user);
+                            } else{
+                                Toast.makeText(RegisterActivity.this, "Authentication failed with following message /n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+        //clicking activity to launch login page
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeKeyboard();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         });
     }
@@ -86,6 +158,7 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
+    // closing keyboard
     final void closeKeyboard(){
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -94,4 +167,25 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
+    void writeNewUser(String UserId, String Email, String Password){
+        User user = new User(Email, Password);
+        mDatabase.child("users").child(UserId).setValue(user);
+    }
+
+}
+
+// user defination for realtime database
+class User {
+    public String Password;
+    public String Email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    // parameterized constructor
+    public User(String Email, String Password) {
+        this.Password = Password;
+        this.Email = Email;
+    }
 }
