77dd65919dcf14b88df1c7c46e5a8fcc94348e6e
Ankit <karandeankit123@gmail.com>
2021-03-13 11:03:16 +0530

    Fragment is added

---
+++ app/src/main/java/com/rajat/projecta/FragmentActivity.java
@@ -0,0 +1,42 @@
+package com.rajat.projecta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        BottomNavigationView bottomnav = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+        bottomnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment select = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        select = new HomeFragment();
+                        break;
+                    case R.id.nav_faq:
+                        select = new FaqFragment();
+                        break;
+                    case R.id.nav_account:
+                        select = new AccountFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,select).commit();
+                return true;
+            }
+        });
+    }
+}
