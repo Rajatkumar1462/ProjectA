ddb2151e936c3d056619c04185237bba3dca29dd
Ankit <karandeankit123@gmail.com>
2021-03-13 11:22:59 +0530

    Fragments changed

---
+++ app/src/main/java/com/rajat/projecta/AccountFragment.java
@@ -0,0 +1,17 @@
+package com.rajat.projecta;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AccountFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_account,container,false);
+    }
+}
