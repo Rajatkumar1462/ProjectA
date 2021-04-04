96e2d71ea782e4dab4c58cbc33664091583912a1
Rajat <rajatleuva1462@gmail.com>
2021-04-04 15:53:34 +0530

    Added some constraints on RegisterActivity.java

--- app/src/main/java/com/rajat/projecta/Fragment/AccountFragment.java
+++ app/src/main/java/com/rajat/projecta/Fragment/AccountFragment.java
@@ -1,19 +1,48 @@
 package com.rajat.projecta.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.rajat.projecta.FragmentActivity;
+import com.rajat.projecta.LoginActivity;
+import com.rajat.projecta.MainActivity;
 import com.rajat.projecta.R;
 
 public class AccountFragment extends Fragment {
     @Nullable
-    @Override
+    private Button meSignOut;
+    private Button meEdit;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account,container,false);
+        View view = inflater.inflate(R.layout.fragment_account,container,false);
+        meSignOut = view.findViewById(R.id.me_signout);
+        meEdit = view.findViewById(R.id.me_edit);
+        meSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+        meEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+
+        return view;
+    }
+
+    private void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
     }
 }
