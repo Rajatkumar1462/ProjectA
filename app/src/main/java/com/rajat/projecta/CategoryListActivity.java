3607e1428ab3caf966461c20bddd823f948fff46
Rajat <rajatleuva1462@gmail.com>
2021-04-06 23:15:12 +0530

    Automatic fetching from firebase in CategoryListActivity.java

--- app/src/main/java/com/rajat/projecta/CategoryListActivity.java
+++ app/src/main/java/com/rajat/projecta/CategoryListActivity.java
@@ -12,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.rajat.projecta.Adapter.ServiceProviderAdapter;
 import com.rajat.projecta.Adapter.ServiceProviderAdapter2;
+import com.rajat.projecta.Fragment.HomeFragment;
 import com.rajat.projecta.Helper.ServiceProviderHelper;
 
 import java.util.ArrayList;
@@ -20,10 +21,11 @@ public class CategoryListActivity extends AppCompatActivity implements ServicePr
 
     //Declaration
     private TextView AtTopName;
-    ArrayList<ServiceProviderHelper> sp_list = new ArrayList<>();
+    ArrayList<ServiceProviderHelper> sp_list2 = new ArrayList<>();
     FirebaseUser user;
 
     private String Type;
+    HomeFragment hf = new HomeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,35 +40,21 @@ public class CategoryListActivity extends AppCompatActivity implements ServicePr
         RecyclerView serviceprovider_full_list = findViewById(R.id.sp_full_list);
         serviceprovider_full_list.setLayoutManager(new GridLayoutManager(this,1));
 
-        sp_list.add(new ServiceProviderHelper(R.drawable.cook,"Suresh","5 star","3 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.cook,"Mamta Devi","4.7 star","1 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.cook,"Gopal","3.7 star","6 Month Experience"));
+        hf.loadValues("Cook", sp_list2,R.drawable.cook);
+        hf.loadValues("Driver", sp_list2,R.drawable.driver);
+        hf.loadValues("Maid", sp_list2,R.drawable.maid);
+        hf.loadValues("Gardener", sp_list2,R.drawable.gardner);
+        hf.loadValues("BabySitter", sp_list2,R.drawable.baby_sitter);
 
-        sp_list.add(new ServiceProviderHelper(R.drawable.driver,"Basant","4.8 star","2.5 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.driver,"Tahir","4.5 star","2 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.driver,"Rajiv Anna","4.4 star","1 year Experience"));
-
-        sp_list.add(new ServiceProviderHelper(R.drawable.maid,"Mala","4.9 star","5 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.maid,"Shakuntala","4.8 star","3.5 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.maid,"Ramu","4.5 star","2 year Experience"));
-
-        sp_list.add(new ServiceProviderHelper(R.drawable.gardner,"Prakash","4.5 star","2 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.gardner,"Raman","4.4 star","1 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.gardner,"Shadique","4.3 star","New"));
-
-        sp_list.add(new ServiceProviderHelper(R.drawable.baby_sitter,"Pragati","4.5 star","3 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.baby_sitter,"Mohini","4 star","2 year Experience"));
-        sp_list.add(new ServiceProviderHelper(R.drawable.baby_sitter,"Rohini","3.8 star","1 year Experience"));
-
-        serviceprovider_full_list.setAdapter(new ServiceProviderAdapter2(sp_list, (ServiceProviderAdapter2.ServiceproviderClickInterface) this));
+        serviceprovider_full_list.setAdapter(new ServiceProviderAdapter2(sp_list2, (ServiceProviderAdapter2.ServiceproviderClickInterface) this));
     }
 
     @Override
     public void onSpItemClick(int position) {
-        String title = sp_list.get(position).getTitle();
-        String rating = sp_list.get(position).getRating();
-        String info = sp_list.get(position).getInfo();
-        int image = sp_list.get(position).getImage();
+        String title = sp_list2.get(position).getTitle();
+        String rating = sp_list2.get(position).getRating();
+        String info = sp_list2.get(position).getInfo();
+        int image = sp_list2.get(position).getImage();
 
         Intent intent = new Intent(CategoryListActivity.this,BookActivity.class);
 
